bfb08539433f7a96ee128b96db208f4f2cbeaae1
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-02 17:31:43 +0600

    Tested the like

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'name1', postsCounter: 0) }
+  let(:post) { user.posts.create(title: 'title', CommentsCounter: 0, LikesCounter: 0) }
+
+  it 'should update post likes counter' do
+    l1 = post.likes.create(user_id: user.id, post_id: post.id)
+    l2 = post.likes.create(user_id: user.id, post_id: post.id)
+    l3 = post.likes.create(user_id: user.id, post_id: post.id)
+    l4 = post.likes.create(user_id: user.id, post_id: post.id)
+    l5 = post.likes.create(user_id: user.id, post_id: post.id)
+
+    post.reload
+
+    expect(post.LikesCounter).to eq 5
+  end
 end
