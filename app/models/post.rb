7123b4ffda80e193036feb843eadd9dd526fee0f
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-02 13:51:32 +0600

    Post validation are created

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   has_many :likes
   after_create :update_user_posts_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :CommentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
