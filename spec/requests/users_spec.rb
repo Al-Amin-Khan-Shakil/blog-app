a28dae3c9d8fcc451dae0096277000f3b1a377e6
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-05 23:54:56 +0600

    Add test for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,36 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    it "works! when for the index page" do
+    it "should be a successful response" do
       get '/users'
       expect(response).to have_http_status(200)
+    end
+
+    it 'should render the index page of users' do
+      get '/users'
+      expect(response).to render_template('users/index')
+    end
+
+    it "should include the placeholder" do
+      get '/users'
+      expect(response.body).to include('Here is the users index page!')
+    end
+  end
+
+  describe 'GET / show' do
+    it 'should be a successfull respose for specfic id' do
+      get '/users/:id'
+      expect(response).to be_successful
+    end
+
+    it 'should render the show page of users' do
+      get '/users/:id'
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include the placeholder' do
+      get '/users/:id'
+      expect(response.body).to include('Here is the users show page!')
     end
   end
 end
