17f139cf12edd7ce4c3dc68769261d04d26985f0
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-08 23:58:08 +0600

    Add new post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,30 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = current_user
     @posts = @user.posts
   end
 
   def show
-    @post = Post.find_by_id(params[:id])
+    @post = current_user.posts.find_by_id(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
