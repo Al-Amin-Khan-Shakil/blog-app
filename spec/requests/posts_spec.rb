a931f27a67bbe1c6b84b502001a5df6d3e3cf176
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-12 02:13:34 +0600

    Fix the previous testing issues

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,37 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  let(:user) { User.create(name: 'user1', postsCounter: 0) }
+  let(:post) { user.posts.create(title: 'title1', CommentsCounter: 0, LikesCounter: 0) }
+
   describe 'GET /index' do
     it 'should be response successull' do
-      get '/users/:id/posts'
+      get "/users/#{user.id}/posts"
       expect(response).to have_http_status(:ok)
     end
 
     it 'should render the posts index file' do
-      get '/users/:id/posts'
+      get "/users/#{user.id}/posts"
       expect(response).to render_template(:index)
     end
 
     it 'should include the placeholder' do
-      get '/users/:id/posts'
-      expect(response.body).to include('Here is the index for posts')
+      get "/users/#{user.id}/posts"
+      expect(response.body).to include('Pagination')
     end
   end
 
   describe 'GET /show' do
     it 'should be a successfull response' do
-      get '/users/:id/posts/:id'
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to be_successful
     end
 
     it 'should render the posts show file' do
-      get '/users/:id/posts/:id'
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to render_template('posts/show')
     end
 
     it 'should include the placeholder' do
-      get '/users/:id/posts/:id'
-      expect(response.body).to include('Here is the show for posts')
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response.body).to include('title1')
     end
   end
 end
