0118905106e7189fbe3833c2ca4d5628a3cfdb68
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-14 14:27:31 +0600

    Create the sign up and sign out

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '3.2.2'
+ruby '3.0.2'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.1.2'
@@ -80,5 +80,3 @@ group :test do
   # gem 'capybara'
   # gem 'selenium-webdriver'
 end
-
-gem "devise", "~> 4.9"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:user_sign_out]
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def user_sign_out
+    sign_out current_user
+    redirect_to new_user_session_path notice: 'Signed out successfully.'
   end
 end
